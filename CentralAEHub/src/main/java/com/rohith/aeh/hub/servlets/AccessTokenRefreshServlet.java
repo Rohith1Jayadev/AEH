1085eb3953deb570e8aa31d14ace58bd1fb21abb
Accolite <Accolite@ROJA-BRL-01>
2018-03-16 11:23:05 +0530

    Added A refresh token validation servlet to handle token refresh requests

---
+++ CentralAEHub/src/main/java/com/rohith/aeh/hub/servlets/AccessTokenRefreshServlet.java
@@ -0,0 +1,26 @@
+package com.rohith.aeh.hub.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/refresh")
+public class AccessTokenRefreshServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		super.doGet(req, resp);
+	}
+
+}
